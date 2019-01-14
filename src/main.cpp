e463365371c9d4c305a6554ac08b3954cf7455bf
f3rdy <f3rdy@users.noreply.github.com>
2019-01-15 00:36:04 +0100

    adding rtc

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <SimpleDHT.h>
+#include <Wire.h>
+#include <RtcDS3231.h>
 
 
 #define CONNECTION_BAUD 57600
@@ -8,11 +10,46 @@
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 SimpleDHT11   dht11(PIN_DHT);
+RtcDS3231<TwoWire> Rtc(Wire);
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char date[11];
+    char time[9];
+
+    snprintf_P(date,
+            countof(date),
+            PSTR("%02u/%02u/%04u"),
+            dt.Day(),
+            dt.Month(),
+            dt.Year());
+    snprintf_P(time,
+            countof(time),
+            PSTR("%02u:%02u:%02u"),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second());
+
+    Serial.print(date); Serial.print(" "); Serial.println(time);
+    lcd.setCursor(8,0); lcd.print(time);
+    lcd.setCursor(6,1); lcd.print(date);
+}
 
 void setup() {
   Serial.begin(CONNECTION_BAUD);
   pinMode(LED_BUILTIN, OUTPUT);
   lcd.begin(16, 2);
+
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Rtc.SetDateTime(compiled);
 }
 
 void loop() {
@@ -26,6 +63,10 @@ void loop() {
   }
 
   lcd.setCursor(0, 0); lcd.print((int) temperature); lcd.print("C");
-  lcd.setCursor(5, 0); lcd.print((int) humidity); lcd.print("%");
+  lcd.setCursor(0, 1); lcd.print((int) humidity); lcd.print("%");
+
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+
   delay(1500);
 }
